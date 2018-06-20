0d324a211abf2fdb0143f3d2cd8895bc6adc96a0
Brenton Turner <30302626+brenton26@users.noreply.github.com>
2018-06-20 15:34:51 -0700

    Add files via upload

---
+++ Categories.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace BlueRibbonsReview.Models
+{
+    public enum Categories
+    {
+        [Display(Name = "Clothing")] Clothing,
+        [Display(Name ="Food & Drink")] FoodAndDrink,
+        [Display(Name = "Health & Beauty")] HealthAndBeauty,
+        [Display(Name = "Home Products")] HomeProducts,
+        [Display(Name = "Children")] Children,
+        [Display(Name = "Technology")] Technology,
+        [Display(Name = "Miscellaneous")] Miscellaneous
+    }
+
+    public static class CategoriesHelper
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType().GetMember(enumValue.ToString())
+                .First().GetCustomAttribute<DisplayAttribute>().GetName();
+        }
+    }
+}
